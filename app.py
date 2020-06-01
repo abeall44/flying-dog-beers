8d82a82d1001ad3abdc8f36d59fa5de90c4c712c
abeall44 <66272753+abeall44@users.noreply.github.com>
2020-06-01 14:56:27 -0400

    Update app.py
    Back to beer with additional button

--- app.py
+++ app.py
@@ -1,49 +1,79 @@
-## IMPORTS #######
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 from dash.dependencies import State, Input, Output
-
 import xlwings as xw
 
 
+########### Define your variables
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 0]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='lightblue'
+color2='darkgreen'
+mytitle='Beer Comparison'
+tabtitle='beer!'
+myheading='Flying Dog Beers'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
+
+########### Set up the chart
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name=label1,
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name=label2,
+    marker={'color':color2}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = mytitle
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+
+########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-
-
-# a = str(wb.sheets['Sheet1'].range('a1').value)
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
 
+########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
+    html.H1(myheading),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
+    ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A('Data Source', href=sourceurl),
     html.Button(id='update-button', n_clicks=0, children='update'),
-
-    html.Div(id = 'output_1')
-
-])
+    ]
+)
 
 @app.callback(
     Output(component_id='output_1', component_property='children'),
     [Input('update-button', 'n_clicks')],
 )
 def update_xl_values_f(n_clicks):
-    if(n_clicks > 0):   
+    if(n_clicks > 0): 
+
         return(n_clicks)
     else:
         return()
- 
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
-
-)
 
 if __name__ == '__main__':
     app.run_server()
